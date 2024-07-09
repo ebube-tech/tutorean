c020415811e918e632f4922465436fb2ef034640
Abhiuday tiwari <104417372+abhiuday23@users.noreply.github.com>
2024-07-10 01:09:23 +0530

    Add files via upload

--- src/components/home/HowItWorks.tsx
+++ src/components/home/HowItWorks.tsx
@@ -23,11 +23,11 @@ function HowItWorks() {
       <Container>
         <div className="pt-[145px]">
           <Heading text="Learn more about tutoring on Tutorean" />
-          <div className="flex justify-between gap-[31px] mt-[61px]">
+          <div className="grid gap-[31px] mt-[61px] grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {howItWorks.map((item, index) => (
               <div
                 key={index}
-                className="w-[420px] bg-[#fff3e5] rounded-[40px] mt-[60px] p-[30px] leading-[24.2px] text-[1.35rem] border-[2px] border-[#F77F00]"
+                className="bg-[#fff3e5] rounded-[40px] p-[30px] leading-[24.2px] text-[1.35rem] border-[2px] border-[#F77F00]"
               >
                 <div className="bg-[#fff3e5] -mt-[30px] relative z-[10] h-[95px] rounded-tr-[40px] rounded-tl-[40px] items-center flex justify-center text-[#F77F00] border-[#F77F00] font-bold text-2xl">
                   <p className="font-[700]">{item.title}</p>
@@ -45,5 +45,6 @@ function HowItWorks() {
 }
 
 export default HowItWorks;
+
 
 
--- src/components/home/TutorSearch.tsx
+++ src/components/home/TutorSearch.tsx
@@ -6,40 +6,35 @@ import Heading from "@/ui/Heading";
 
 function TutorSearch() {
   return (
-    <div className="pt-[0px]">
+    <div className="pt-0">
       <Container>
-        <div className="w-[100%] rounded-[30px] pt-[46px] pb-[53px]">
-          
+        <div className="w-full rounded-[30px] pt-[46px] pb-[53px]">
+          {/* First Image Section */}
+          <div className="mt-[57px] px-[16px] md:px-[34px] lg:px-[68px] rounded-[30px] w-full flex items-center justify-between">
+            <img
+              src="/images/middlewoman.png"
+              alt="Description of your image"
+              className="w-full h-auto object-cover rounded-[30px]"
+            />
+          </div>
 
+          {/* Second Image Section */}
+          <div className="mt-[57px] px-[16px] md:px-[34px] lg:px-[68px] rounded-[30px] w-full flex items-center justify-between">
+            <img
+              src="/images/middlewoman2.png"
+              alt="Description of your image"
+              className="w-full h-auto object-cover rounded-[30px]"
+            />
+          </div>
 
-        <div className="mt-[57px] px-[68px] rounded-[30px] w-[100%] flex h-[121px] items-center justify-between">
-  <img
-    src="/images/middlewoman.png"
-    alt="Description of your image"
-    className="w-full h-full object-cover rounded-[30px]"
-  />
-  
-</div>
-<div className="mt-[57px] px-[68px] rounded-[30px] w-[100%] flex h-[121px] items-center justify-between">
-  <img
-    src="/images/middlewoman2.png"
-    alt="Description of your image"
-    className="w-full h-full object-cover rounded-[30px]"
-  />
-  
-</div>
-<div className="mt-[57px] px-[68px] rounded-[30px] w-[100%] flex h-[121px] items-center justify-between">
-  <img
-    src="/images/middlewoman3.png"
-    alt="Description of your image"
-    className="w-full h-full object-cover rounded-[30px]"
-  />
-  
-</div>
-
-          
-        
-
+          {/* Third Image Section */}
+          <div className="mt-[57px] px-[16px] md:px-[34px] lg:px-[68px] rounded-[30px] w-full flex items-center justify-between">
+            <img
+              src="/images/middlewoman3.png"
+              alt="Description of your image"
+              className="w-full h-auto object-cover rounded-[30px]"
+            />
+          </div>
         </div>
       </Container>
     </div>
