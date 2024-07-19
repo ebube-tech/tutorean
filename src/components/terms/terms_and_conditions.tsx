989162290b29040c6a08e0b39ed6950ed2a25b0a
Paul Ndalila <paulndalila001@gmail.com>
2024-07-20 00:09:34 +0300

    terms page completed

---
+++ src/components/terms/terms_and_conditions.tsx
@@ -0,0 +1,22 @@
+export default function Terms_and_conditions (){
+  return (
+    <div className="lg:my-10 my-6 lg:mx-6 mx-2 border-2 border-[#F77F00] bg-white lg:px-8 px-6 py-6 rounded-[30px]">
+        <h1 className="font-bold text-[#F77F00] text-2xl">Terms & Conditions</h1>
+        <div className="my-2 text-md h-60 overflow-auto">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente unde voluptatum consequatur expedita vitae sit in et aspernatur exercitationem veritatis maiores, ea adipisci explicabo corporis inventore voluptatem dicta. Quibusdam, dolorum.</p>
+        </div>
+        <div className="flex items-center justify-end me-10 gap-4 font-bold py-4">
+            <button className="text-[#F77F00] px-[10px] py-[2px] rounded">Cancel</button>
+            <button className="bg-[#F77F00] text-white px-[30px] py-[2px] rounded">Agree</button>
+        </div>
+    </div>
+  )
+}
