ff93b5d99d8fd62efc1ff032e9dd90eb8073d040
oghomena <105161137+GeekyOG@users.noreply.github.com>
2024-07-23 17:10:57 +0100

    header fix

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 import MobileNav from "@/layout/MobileNav";
@@ -18,7 +19,9 @@ function Header() {
       </div>
 
       <div className="hidden lg:flex items-center justify-between h-[110px] mx-auto max-w-[1280px] px-[24px] lg:px-50px">
-        <Image src="/images/logo.svg" alt={""} width={155} height={40} />
+        <Link href="/">
+          <Image src="/images/logo.svg" alt={""} width={155} height={40} />
+        </Link>
 
         <div className=" flex gap-[30px] items-center">
           {NavItems.map((navItem, index) => (
--- src/ui/NavItem.tsx
+++ src/ui/NavItem.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import clsx from "clsx";
 import Link from "next/link";
 import React, { FunctionComponent } from "react";
@@ -14,8 +16,17 @@ const NavItem: FunctionComponent<NavItemProps> = ({
 }) => {
   return (
     <Link href={href} className={clsx("", className)}>
-      <div className="bg-[#F77F00] px-[44px] py-[11px] bg-[#F77F007A] rounded-[18px]">
-        <p className="text-[#000] font-[700]">{label}</p>
+      <div
+        className="bg-[#F77F00] bg-[#F77F007A] rounded-[18px]"
+        style={{
+          height: 46,
+          display: "flex",
+          alignItems: "center",
+          padding: "0px 15px",
+          borderRadius: 10,
+        }}
+      >
+        <p className="text-[#000] font-[700] text-[0.865rem]">{label}</p>
       </div>
     </Link>
   );
